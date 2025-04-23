750fa1d8196a5193a40a415380899d2d561dfb1b
Kyy <127030467+Riski0929@users.noreply.github.com>
2025-04-23 21:02:16 +0800

    Create igdl.js

---
+++ api/igdl.js
@@ -0,0 +1,70 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+module.exports = async (req, res) => {
+  const { url } = req.query;
+  if (!url) {
+    return res
+      .status(406)
+      .setHeader('Content-Type', 'application/json')
+      .send(JSON.stringify({
+        status: false,
+        creator: 'Kyy',
+        code: 406,
+        message: 'masukkan parameter url'
+      }, null, 2));
+  }
+
+  try {
+    const { data } = await axios.post('https://yt1s.io/api/ajaxSearch', new URLSearchParams({
+      q: url,
+      w: '',
+      p: 'home',
+      lang: 'en'
+    }), {
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Origin': 'https://yt1s.io',
+        'Referer': 'https://yt1s.io/',
+        'User-Agent': 'Postify/1.0.0'
+      }
+    });
+
+    const $ = require('cheerio').load(data.data);
+    let result = $('a.abutton.is-success.is-fullwidth.btn-premium').map((_, el) => ({
+      title: $(el).attr('title'),
+      url: $(el).attr('href')
+    })).get();
+
+    if (result.length === 0) {
+      return res
+        .status(404)
+        .setHeader('Content-Type', 'application/json')
+        .send(JSON.stringify({
+          status: false,
+          creator: 'Kyy',
+          code: 404,
+          message: 'Media tidak ditemukan atau url tidak valid'
+        }, null, 2));
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    return res.status(200).send(JSON.stringify({
+      status: true,
+      creator: 'Kyy',
+      result
+    }, null, 2));
+  } catch (e) {
+    return res
+      .status(500)
+      .setHeader('Content-Type', 'application/json')
+      .send(JSON.stringify({
+        status: false,
+        creator: 'Kyy',
+        code: 500,
+        message: `Terjadi kesalahan: ${e.message}`
+      }, null, 2));
+  }
+};
