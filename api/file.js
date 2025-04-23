32b63df2a5810e22c6586f47bff92858952238b7
Kyy <127030467+Riski0929@users.noreply.github.com>
2025-04-23 19:38:48 +0800

    Update file.js

--- api/file.js
+++ api/file.js
@@ -1,1 +1,84 @@
 
+const axios = require('axios');
+
+module.exports = async (req, res) => {
+  const { url } = req.query;
+  if (!url) return res.status(400).json({ status: false, message: 'Missing url parameter' });
+
+  try {
+    let data = []
+    function formatNumber(integer) {
+      let numb = parseInt(integer)
+      return Number(numb).toLocaleString().replace(/,/g, '.')
+    }
+
+    function formatDate(n, locale = 'en') {
+      let d = new Date(n * 1000)
+      return d.toLocaleDateString(locale, {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+      })
+    }
+
+    let domain = 'https://www.tikwm.com/api/';
+    let resTik = await (await axios.post(domain, {}, {
+      headers: {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'User-Agent': 'Mozilla/5.0',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Referer': 'https://www.tikwm.com/'
+      },
+      params: {
+        url: url,
+        hd: 1
+      }
+    })).data.data
+
+    if (resTik && !resTik.size && !resTik.wm_size && !resTik.hd_size) {
+      resTik.images.map(v => data.push({ type: 'photo', url: v }))
+    } else {
+      if (resTik.wmplay) data.push({ type: 'watermark', url: resTik.wmplay })
+      if (resTik.play) data.push({ type: 'nowatermark', url: resTik.play })
+      if (resTik.hdplay) data.push({ type: 'nowatermark_hd', url: resTik.hdplay })
+    }
+
+    return res.json({
+      status: true,
+      title: resTik.title,
+      taken_at: formatDate(resTik.create_time),
+      region: resTik.region,
+      id: resTik.id,
+      duration: resTik.duration + ' Seconds',
+      cover: resTik.cover,
+      data: data,
+      music_info: {
+        id: resTik.music_info.id,
+        title: resTik.music_info.title,
+        author: resTik.music_info.author,
+        album: resTik.music_info.album || null,
+        url: resTik.music || resTik.music_info.play
+      },
+      stats: {
+        views: formatNumber(resTik.play_count),
+        likes: formatNumber(resTik.digg_count),
+        comment: formatNumber(resTik.comment_count),
+        share: formatNumber(resTik.share_count),
+        download: formatNumber(resTik.download_count)
+      },
+      author: {
+        id: resTik.author.id,
+        fullname: resTik.author.unique_id,
+        nickname: resTik.author.nickname,
+        avatar: resTik.author.avatar
+      }
+    })
+  } catch (e) {
+    return res.status(500).json({ status: false, message: e.message })
+  }
+          }
