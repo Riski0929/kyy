b8c04442711bdb89c201dd09cde32219d649a05f
Kyy <127030467+Riski0929@users.noreply.github.com>
2025-04-24 12:28:57 +0800

    Update igdl.js

--- api/igdl.js
+++ api/igdl.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-const cheerio = require('cheerio');
 
 module.exports = async (req, res) => {
   const { url } = req.query;
@@ -16,52 +15,13 @@ module.exports = async (req, res) => {
   }
 
   try {
-    const { data } = await axios.post('https://yt1s.io/api/ajaxSearch', new URLSearchParams({
-      q: url,
-      w: '',
-      p: 'home',
-      lang: 'en'
-    }), {
+    const { data } = await axios.get('https://api.fabdl.com/spotify/get?url=' + url, {
       headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Origin': 'https://yt1s.io',
-        'Referer': 'https://yt1s.io/',
-        'User-Agent': 'Postify/1.0.0'
+        'content-type': 'application/json'
       }
     });
 
-    const $ = cheerio.load(data.data);
-    const resultArray = $('a.abutton.is-success.is-fullwidth.btn-premium').map((_, el) => ({
-      url: $(el).attr('href')
-    })).get();
-
-    if (resultArray.length < 2) {
-      return res
-        .status(404)
-        .setHeader('Content-Type', 'application/json')
-        .send(JSON.stringify({
-          status: false,
-          creator: 'Kyy',
-          code: 404,
-          message: 'Media tidak ditemukan atau url tidak valid'
-        }, null, 2));
-    }
-
-    const mediaUrl = resultArray[1].url;
-    const head = await axios.head(mediaUrl);
-    const contentType = head.headers['content-type'] || '';
-
-    let type = 'unknown';
-    let result = {};
-
-    if (contentType.includes('video')) {
-      type = 'video';
-      result.video = mediaUrl;
-    } else if (contentType.includes('image')) {
-      type = 'image';
-      result.images = resultArray.slice(1).map(r => r.url);
-    }
+    const { data: res2 } = await axios.get(`https://api.fabdl.com/spotify/mp3-convert-task/${data.result.gid}/${data.result.id}`);
 
     return res
       .status(200)
@@ -69,8 +29,13 @@ module.exports = async (req, res) => {
       .send(JSON.stringify({
         status: true,
         creator: 'Kyy',
-        type,
-        result
+        type: 'audio',
+        result: {
+          title: data.result.name,
+          duration: data.result.duration_ms,
+          cover: data.result.image,
+          download: "https://api.fabdl.com" + res2.result.download_url
+        }
       }, null, 2));
 
   } catch (e) {
